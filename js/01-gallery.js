ef3997982a4e930015c6b302c609657f3449d216
Andrii Horbovskyi <117573529+g0rbochek@users.noreply.github.com>
2023-08-12 12:47:07 +0200

    hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const ulItems = document.querySelector(".gallery");
+console.log(ulItems);
+
+ulItems.insertAdjacentHTML("beforeend", creatMarkup(galleryItems));
+
+function creatMarkup(obj) {
+  return obj
+    .map(
+      ({ description, original, preview }) =>
+        `
+        <li class="gallery__item">
+            <a class="gallery__link" href="">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>`
+    )
+    .join("");
+}
+
+ulItems.addEventListener("click", onClickGetLargePicture);
+
+function onClickGetLargePicture(evt) {
+  evt.preventDefault();
+  if (evt.target === evt.currentTarget) {
+    return;
+  }
+  const originalImg = evt.target.dataset.source;
+  const instance = basicLightbox.create(`<img src="${originalImg}"/>`);
+  instance.show();
+
+  ulItems.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const ulItems = document.querySelector(".gallery");
+console.log(ulItems);
+
+ulItems.insertAdjacentHTML("beforeend", creatMarkup(galleryItems));
+
+function creatMarkup(obj) {
+  return obj
+    .map(
+      ({ description, original, preview }) => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`
+    )
+    .join("");
+}
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+// ulItems.addEventListener("click", onClickGetLargePictureAndFunctionale);
+
+// function onClickGetLargePictureAndFunctionale(evt) {
+//   evt.preventDefault();
+//   const tar = evt.target;
+
+//   if (tar === evt.currentTarget) {
+// return;
+//   }
+//   const currentItem = tar.closest(".galery__item");
+//   const { source } = tar.dataset;
+//   const item = galleryItems.find((item) => item.original === source);
+
+//   const instance = basicLightbox.create(
+//     `<img src="${item.original}" alt="${item.description}" width="800" height="600">`
+//   );
+//   instance.show();
+
+//   const instance = basicLightbox.create(
+// `<img src="${item.original}" alt="${item.description}" width="800" heigth="600" />`
+//   );
+// }
